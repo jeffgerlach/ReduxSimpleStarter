2bca529074c3aafc3885982af51aea0951baf6db
jgerlach <jgerlach@tmcdesign.com>
2017-02-13 16:38:21 -0700

    Implement simple content test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,20 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// Use 'describe' to group together similar tests
 describe('App' , () => {
   let component;
 
   beforeEach(() => {
     component = renderComponent(App);
   });
-
+// use 'it' to test a single attribute of a target
   it('renders something', () => {
     expect(component).to.exist;
+  });
+
+  it('shows the correct text', () => {
+    // use 'expect' to make an assertion about a target
+    expect(component).to.contain('React simple starter');
   });
 });
