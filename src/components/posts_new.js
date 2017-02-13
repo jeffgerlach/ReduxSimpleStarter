d29ec89aa7295b84ae7ddb80ad3f7895b86d84c9
Jeff Gerlach <jeffgerlach06@gmail.com>
2017-02-12 21:20:14 -0700

    Implement redirect to index on post submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context..router.push with the
+        // new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     //const handleSubmit = this.props.handleSubmit; //ES6 syntax below
     //const title = this.props.fields.title; //more ES6 below
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create A New Post</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
