babc3b52d0757eedc344dd1fe2bcfd669bd9f876
jgerlach <jgerlach@tmcdesign.com>
2017-02-15 13:37:11 -0700

    Finish simple auth HOC example

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    } // can now access Authentication.contextTypes
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      return <ComposedComponent {...this.props} />
+      return <ComposedComponent {...this.props} />;
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
 
 /*
