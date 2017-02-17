9af8fba5db1301a481e61e7bf2761da9036c2a40
jgerlach <jgerlach@tmcdesign.com>
2017-02-17 12:04:55 -0700

    Implement header component for sign in out and up. Create signup reducer and sign in and out components.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,36 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
+  // can return a function with redux-thunk
   return function(dispatch) {
     // submit email/password to the server
     // { email: email, password: password }
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // if request is good ...
-    // - update state to indicate user is authenticated
-    // - save the JWT token
-    // - redirect to the route '/feature'
-
-    // if request is bad...
-    // - show an error to the user
+    // axios returns a promise
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // if request is good ...
+        // - update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // if request is bad...
+        // - show an error to the user
+        dispatch(authError('Bad Login Info'));
+      });
   }
+}
 
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -10,6 +10,16 @@ class Signin extends Component {
     this.props.signinUser({ email, password })
   }
 
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
   render() {
     const { handleSubmit, fields: { email, password }} = this.props;
 
@@ -21,12 +31,17 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <Field name="password" component="input" className="form-control" />
+          <Field name="password" type="password" component="input" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
 }
 
 Signin = reduxForm({
@@ -34,4 +49,4 @@ Signin = reduxForm({
   fields: ['email', 'password']
 })(Signin);
 
-export default Signin = connect(null, actions)(Signin);
+export default Signin = connect(mapStateToProps, actions)(Signin);
